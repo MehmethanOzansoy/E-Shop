22430ec54370645130a53a1d4d9c412fb995b145
mehmethanozansoy98@gmail.com <94033888+MehmethanOzansoy@users.noreply.github.com>
2022-08-23 16:31:29 +0300

    commit4

--- resources/views/home/test.blade.php
+++ resources/views/home/test.blade.php
@@ -5,7 +5,7 @@
 <h1>My First Test</h1>
 <p>My first paragraph.</p>
 
-<form action="/save",method="post">
+<form action="/save",method="post ">
  @csrf
   <label for="fname">First name:</label><br>
   <input type="text" id="fname" name="fname"><br>
--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,18 +1,36 @@
-<html>
-    <head>
-        <title>App Name - @yield('title')</title>
-        @yield('head')
-    </head>
-    <body>
-        <h1>Header</h1>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
- 
-        <div class="container">
-            @yield('content')
-        </div>
-        <h1>Footer</h1>
-        @yield('foot')
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>@yield("title")</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="{{asset('assets')}}img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{asset('assets')}}/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{asset('assets')}}/css/style.css" rel="stylesheet">
+    @yield("head")
+</head>
+@include("home.header")
+
+@section('sidebar')
+    @include("home.sidebar")
+@show
+
+@yield('content')
+
+@include("home.footer")
+@yield('foot')
+</body>
 </html>     
