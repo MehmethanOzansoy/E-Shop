2b4ad2e4e04c8566c27492791b3b00696c5b7a57
mehmethanozansoy98@gmail.com <94033888+MehmethanOzansoy@users.noreply.github.com>
2022-08-25 10:37:45 +0300

    5.commit

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -1,6 +1,5 @@
     <!-- Navbar Start -->
     <div class="content">
-    
         <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
             <a href="{{asset('assets')}}/admin/index.html" class="navbar-brand d-flex d-lg-none me-4">
                 <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
@@ -89,4 +88,4 @@
             </div>
         </nav>
     
-    <!-- Navbar End -->
+     <!-- Navbar End -->
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,4 +1,3 @@
-    <div class="content">
         <!-- Spinner Start -->
         <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
@@ -50,5 +49,4 @@
                 </div>
             </nav>
         </div>
-    </div>
 <!-- Sidebar End -->
